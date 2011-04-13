c6b932bfa9dc24a375025c29d9d82ce2ee918acc
dirk luesebrink <dirk.luesebrink@artcom.de>
2011-04-13 14:55:49 +0200

    fixed: -a was wrong

--- packages/rvm.sh
+++ packages/rvm.sh
@@ -1,15 +1,15 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 function clear-rails-and-ruby-package() {
-    unalias -a r
-    unalias -a testrb
-    unalias -a ri
-    unalias -a rdoc
-    unalias -a rake
-    unalias -a irb
-    unalias -a gem
-    unalias -a er
+    unalias r
+    unalias testrb
+    unalias ri
+    unalias rdoc
+    unalias rake
+    unalias irb
+    unalias gem
+    unalias er
 }
-clear-rails-and-ruby-package
+clear-rails-and-ruby-package > /dev/null 2>&1 
 
 alias r=ruby
